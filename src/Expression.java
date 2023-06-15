2c6465d99abf69a6ba2bf01f3b76d5cc6e63491e
Vladimir Drozdov <v.drozdov@sftpro.com>
2023-06-15 13:40:52 +0300

    Added expression class

--- src/Expression.java
+++ src/Expression.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Expression {
+public class Expression {
+    private final int firstNum, secondNum;
+    private final boolean isRomanResult;
+    private final Byte operation;
+
+    public Expression(int firstNum, int secondNum, Byte operation, boolean isRomanResult) {
+        this.firstNum = firstNum;
+        this.secondNum = secondNum;
+        this.isRomanResult = isRomanResult;
+        this.operation = operation;
+    }
+
+    public int[] getNumbers() {
+        return new int[]{ this.firstNum, this.secondNum };
+    }
+    public byte getOperation() {
+        return this.operation;
+    }
+    public boolean checkIsRomanResult() {
+        return this.isRomanResult;
+    }
 }
