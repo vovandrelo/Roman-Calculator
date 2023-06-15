96ccb5de3581461fe2bfa353ff70e403cc6cb8f3
Vladimir Drozdov <v.drozdov@sftpro.com>
2023-06-15 13:40:33 +0300

    Edited converter logic

--- src/Converter.java
+++ src/Converter.java
@@ -1,10 +1,5 @@
-package com.vov;
-
 public class Converter {
-    private int firstNum = -1, secondNum = -1;
-    private boolean isRomanResult = false;
-    private Byte operation;
-    private static final String[] romanNumbers  = {
+    private static final String[] ROMAN_NUMBERS = {
             "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X",
             "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
             "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX",
@@ -17,81 +12,44 @@ public class Converter {
             "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C"
     };
 
-
-    public void parseInputStr(String expression) throws Exception {
+    public Expression parseInputStr(String expression) {
         String[] expressionElements = expression.split(" ");
+        byte operation;
+        int firstNumber, secondNumber;
+        boolean isRomanResult = false;
 
-        if (expressionElements.length != 3) {
-            throw new Exception("Error! Введено некорректное выражение");
+        operation = expressionElements[1].getBytes()[0];
+
+        try {
+            firstNumber = Integer.parseInt(expressionElements[0]);
+            secondNumber = Integer.parseInt(expressionElements[2]);
+        } catch (NumberFormatException error) {
+            isRomanResult = true;
+            firstNumber = -1;
+            secondNumber = -1;
+            for (int i = 0; i < 10; i++) {
+                if (expressionElements[0].equals(ROMAN_NUMBERS[i])) {
+                    firstNumber = i + 1;
+                }
+                if (expressionElements[2].equals(ROMAN_NUMBERS[i])) {
+                    secondNumber = i + 1;
+                }
+            }
         }
 
-        int firstNumber = parseNumber(expressionElements[0]);
-        int secondNumber = parseNumber(expressionElements[2]);
-        byte operation = expressionElements[1].getBytes()[0];
-
-        boolean firstNumberIsRoman = checkNumberIsRoman(expressionElements[0]);
-        boolean secondNumberIsRoman = checkNumberIsRoman(expressionElements[2]);
-
-        if (firstNumberIsRoman != secondNumberIsRoman) {
-            throw new Exception("Error! Числа должны быть единой системы счисления");
-        }
-
-        if (firstNumber < 1 || firstNumber > 10 || secondNumber < 1 || secondNumber > 10) {
-            throw new Exception("Error! Числа должны находиться в диапазоне от 1 до 10");
-        }
-
-        if (operation != '+' && operation != '-' && operation != '*' && operation != '/') {
-            throw new Exception("Error! Некорректная операция");
-        }
-
-        this.firstNum = firstNumber;
-        this.secondNum = secondNumber;
-        this.isRomanResult = secondNumberIsRoman;
-        this.operation = operation;
-    }
-    private int parseNumber(String someNum) throws Exception {
-        for (int i = 0; i < 10; i++) {
-            String index = Integer.toString(i + 1);
-
-            if (someNum.equals(index)) return Integer.parseInt(someNum);
-            if (someNum.equals(romanNumbers[i])) return this.firstNum = i + 1;
-        }
-        throw new Exception("Error! Некорректное число");
+        return new Expression(firstNumber, secondNumber, operation, isRomanResult);
     }
 
-    private boolean checkNumberIsRoman(String someNum) throws Exception {
-        for (int i = 0; i < 10; i++) {
-            String index = Integer.toString(i + 1);
-
-            if (someNum.equals(index)) return false;
-            if (someNum.equals(romanNumbers[i])) return true;
-        }
-        throw new Exception("Error! Некорректное число");
-    }
-
-    public String parseResult(int result) throws Exception {
-        if (result < 1 && this.isRomanResult) {
-            throw new Exception("Error! Римская система счисления неподдерживает отрицательные числа");
-        }
-
+    public String parseResult(int result, boolean isRomanResult) {
         String finalResult = Integer.toString(result);
 
-        if (this.isRomanResult) {
-            for (int i = 0; i < romanNumbers.length; i++) {
-                String index = Integer.toString(i + 1);
-
-                if (finalResult.equals(index)) return romanNumbers[i];
+        if (isRomanResult) {
+            for (int i = 0; i < ROMAN_NUMBERS.length; i++) {
+                if (finalResult.equals(Integer.toString(i + 1))) return ROMAN_NUMBERS[i];
             }
         }
 
         return finalResult;
-    }
-
-    public int[] getNumbers() {
-        return new int[]{ this.firstNum, this.secondNum };
-    }
-    public byte getOperation() {
-        return this.operation;
     }
 }
 
